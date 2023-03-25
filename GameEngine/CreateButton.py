44a8e3c21a91e89f933bab394403bb3772772ffa
remekozicki <remigiusz.kozicki@gmail.com>
2023-03-25 19:47:16 +0100

    create buttons 2

--- GameEngine/CreateButton.py
+++ GameEngine/CreateButton.py
@@ -2,5 +2,13 @@ import pygame
 
 class Button:
 
-    def __init__(self,x ,y,):
-        self.button
+    def __init__(self, x, y, width, height, text, on_click_functon=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onClickFuncton = on_click_functon
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        font = pygame.font.SysFont('Arial', 40)
+        self.buttonSurf = font.render(text, True, (20, 20, 20))
